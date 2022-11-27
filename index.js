3dbc39ef3c0ac10124cd5a54519a2241ae389629
CrtdLunacy <crtdlunacy@gmail.com>
2022-11-28 00:24:24 +0400

    fix errors9

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ bot.on('message', async (msg) => {
     })
   }
 
-  if (msg.web_app_data.data) {
+  if (msg?.web_app_data?.data) {
     try {
-      const data = JSON.parse(msg.web_app_data.data);
+      const data = JSON.parse(msg?.web_app_data?.data);
       await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
-      await bot.sendMessage(chatId, 'Ваша страна ' + data.country);
-      await bot.sendMessage(chatId, 'Ваша улица ' + data.street);
+      await bot.sendMessage(chatId, 'Ваша страна ' + data?.country);
+      await bot.sendMessage(chatId, 'Ваша улица ' + data?.street);
 
       setTimeout( async () => {
         await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
